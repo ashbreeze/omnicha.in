30076edb15913d13f4ef829064c9b93a8cde1af1
Abraham Lincoln <robbiehotz13@gmail.com>
2014-11-05 23:16:01 -0600

    Create 404.php

---
+++ www/js/pages/404.php
@@ -0,0 +1,10 @@
+<?php
+get_header($pages, $currentpage, "Error: 404", "Page not found");
+?>
+<div class="container">
+	<h3>Error: 404</h3>
+	Sorry, we can't find the page you are looking for.
+</div>
+<?php
+get_footer();
+?>
