ed152e0ed2816c7e222d5e544d6f6275b375ed82
Abraham Lincoln <robbiehotz13@gmail.com>
2014-11-05 23:43:46 -0600

    Update db.php

--- www/theme/safe/db.php
+++ www/theme/safe/db.php
@@ -1,4 +1,16 @@
 <?php
+/* Copyright (c) 2014 by the Omnicoin Team.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
 $abedatabase = mysqli_connect('localhost', 'abe', '*********************************************', 'abe');
 $database = mysqli_connect('localhost', 'omnichain', '*********************************************', 'omnichain');
 
